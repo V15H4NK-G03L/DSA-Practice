4cab1001fda1e42fc1e5040a2c88a96cef804d4f
Vishank Goel <vishankgoel11@gmail.com>
2024-12-03 17:56:10 +0530

    https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0

---
+++ GFG_Largest-Element-in-Array.cpp
@@ -0,0 +1,54 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+#include<algorithm>
+ 
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        // code here
+        int n = arr.size();
+        //cout << n << endl;
+        
+        sort(arr.begin(),arr.end());
+        
+        return arr[n-1];
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--) {
+        string input;
+        int num;
+        vector<int> arr;
+
+        getline(cin, input);
+        stringstream s2(input);
+        while (s2 >> num) {
+            arr.push_back(num);
+        }
+
+        Solution ob;
+        cout << ob.largest(arr) << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
