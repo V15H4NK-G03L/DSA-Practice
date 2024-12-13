bb0d48062b439e40fcbe8d83a70fc648ec500616
Vishank Goel <vishankgoel11@gmail.com>
2024-12-14 00:29:32 +0530

    https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/

---
+++ LC_Find-Minimum-In-Sorted-Array.cpp
@@ -0,0 +1,16 @@
+#include <limits.h>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int ans = nums[0];
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]<=ans){
+                ans = nums[i];
+            }
+        }
+        return ans;
+    }
+};
