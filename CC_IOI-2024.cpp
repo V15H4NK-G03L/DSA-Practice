5d48d3ea43933e9c4fb4c89dfde175f52a986cea
Vishank Goel <vishankgoel11@gmail.com>
2024-12-16 23:50:02 +0530

    https://www.codechef.com/problems/IOI2024

---
+++ CC_IOI-2024.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int d;
+	cin >> d;
+	if(d>=1 && d<=8){cout << "YES" << endl;}
+	else{cout << "NO" << endl;}
+
+}
