cd4ea2f03662e909dc04dc1d245c5f970aaece5c
Vishank Goel <vishankgoel11@gmail.com>
2024-12-21 17:25:25 +0530

    Upsolve-1

--- LC_Sort-Colors.cpp
+++ LC_Sort-Colors.cpp
@@ -5,19 +5,36 @@ using namespace std;
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        vector<int> first,last;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0){
-                first.insert(first.begin(),0);
+        // vector<int> first,last;
+        // for(int i=0;i<nums.size();i++){
+        //     if(nums[i]==0){
+        //         first.insert(first.begin(),0);
+        //     }
+        //     else if(nums[i]==1){
+        //         first.push_back(1);
+        //     }
+        //     else{
+        //         last.push_back(2);
+        //     }
+        // }
+        // first.insert(first.end(),last.begin(),last.end());
+        // nums = first;
+
+        //Using Two Pointer Approach :
+        int left=0,right=nums.size()-1,index=0;
+        while(index<=right){
+            if(nums[index]==0){
+                swap(nums[index],nums[left]);
+                left++;
+                index++;
             }
-            else if(nums[i]==1){
-                first.push_back(1);
+            else if(nums[index]==2){
+                swap(nums[index],nums[right]);
+                right--;
             }
             else{
-                last.push_back(2);
+                index++;
             }
         }
-        first.insert(first.end(),last.begin(),last.end());
-        nums = first;
     }
 };
