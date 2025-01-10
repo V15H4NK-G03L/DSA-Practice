2e1ed7b5c1b478ccd2b694c91df1777df24c9ff9
Vishank Goel <vishankgoel11@gmail.com>
2025-01-10 22:42:49 +0530

    Upsolved using Binary Search

--- LC_Arranging-Coins.cpp
+++ LC_Arranging-Coins.cpp
@@ -5,12 +5,35 @@ using namespace std;
 class Solution {
 public:
     int arrangeCoins(int n) {
-        int a=1,count=0;
-        while(a<=n){
-            count++;
-            n -= a;
-            a++;
+        // int a=1,count=0;
+        // while(a<=n){
+        //     count++;
+        //     n -= a;
+        //     a++;
+        // }
+        // return count;
+
+        long ans=0;
+        long start = 0;
+        long end = n;
+        long mid = (start+end)/2;
+
+        while(start<=end){
+            long long check = (mid * (mid+1))/2;
+            if(check==n){
+                return mid;
+            }
+            else if(check<n){
+                ans=mid;
+                start = mid+1;
+            }
+            else{
+                end = mid-1;
+            }
+
+            mid = (start+end)/2;
         }
-        return count;
+        
+        return ans;
     }
 };
